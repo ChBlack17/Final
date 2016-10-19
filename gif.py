daa30496125597e0e0ccde757ca8d667ed82c5d0
ChBlack17 <ChBlack17@gsb.columbia.edu>
2016-10-19 11:38:47 -0400

    Update gif.py

--- gif.py
+++ gif.py
@@ -1,41 +1,31 @@
 from flask import Flask, render_template, request
 import giphypop
 import os
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
-import urllib,json
 
 app = Flask(__name__)
 
-# API Key from Giphy
-gif_key = os.environ['GIPHY_PUBLIC_API']
+# HTML Pages:
 
-# HTML Pages
+# Index
 @app.route('/')
 def index():
 	return render_template('index.html') 
 
-@app.route('/results')
-def results(gif_key):
-	# do search
-	gifmedia = [] # Make empty list
-	gifurl = []
-	gifs = request.values.get("keyword") # You should define search key word as 'keyword' on html file.
-	results = g.search(gifs) # Search gifs and put it into results 
-
-	for result in results:
-		gifmedia.append(result.media_url)
-		gifurl.append(results.url)
-
-
-	return render_template('results.html', media=gifmedia, url=gifurl)
- 
+# About
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#Results (Search from Giphy)
+@app.route('/results')
+def results():
+	gifs = request.values.get('name') 
+	giphyobject = giphypop.Giphy()
+	results = giphyobject.search(gifs)
+	return render_template('results.html', results=results)	 
+
+# These next lines are needed for Heroku:
 if __name__ == '__main__':
 
-	port = int(os.environ.get("PORT", 5000)) # These next 2 lines are needed for Heroku 
+	port = int(os.environ.get("PORT", 5000))  
 	app.run(host="0.0.0.0", port=port)
-
